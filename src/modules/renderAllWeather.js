9d352e73d15f24cf2e6c21ff8da95c1b4bf430b3
futurebound <mitchellm1618@gmail.com>
2024-04-19 11:15:19 -0700

    feat: rendering of forecast data and icons

--- src/modules/renderAllWeather.js
+++ src/modules/renderAllWeather.js
@@ -15,7 +15,9 @@ const renderAllWeather = () => {
         const { condition, country, currentTemp, forecast, name, region } =
           response;
         const currentDiv = document.createElement('div');
-        const forecastDiv = document.createElement('div');
+        const forecastDiv = renderForecast(forecast);
+
+        contentDiv.appendChild(forecastDiv);
       }
     });
   });
@@ -26,6 +28,40 @@ const renderAllWeather = () => {
 
 const renderForecast = (forecast) => {
   const forecastDiv = document.createElement('div');
+  const days = forecast.map((day) => {
+    const dayDiv = document.createElement('div');
+    dayDiv.className = 'day';
+    console.log(day);
+
+    const date = document.createElement('p');
+    date.textContent = day.date;
+
+    const icon = document.createElement('img');
+    icon.src = day.condition.icon;
+
+    const description = document.createElement('p');
+    description.textContent = day.condition.text;
+
+    const maxTemp = document.createElement('p');
+    maxTemp.textContent = `High: ${day.maxTemp}`;
+
+    const minTemp = document.createElement('p');
+    minTemp.textContent = `Low: ${day.minTemp}`;
+
+    dayDiv.appendChild(date);
+    dayDiv.appendChild(icon);
+    dayDiv.appendChild(description);
+    dayDiv.appendChild(maxTemp);
+    dayDiv.appendChild(minTemp);
+
+    return dayDiv;
+  });
+
+  days.forEach((dayDiv) => {
+    forecastDiv.append(dayDiv);
+  });
+
+  return forecastDiv;
 };
 
 export default renderAllWeather;
